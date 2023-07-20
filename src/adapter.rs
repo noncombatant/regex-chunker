03b8a1550b2ad2c80e1c6b1dcdbd1ff42e5840b1
Dan <dx2718@gmail.com>
2023-07-19 20:10:24 -0400

    resolved; need to reimplement StringChunker and do a custom async chunker, too

--- src/adapter.rs
+++ src/adapter.rs
@@ -19,22 +19,17 @@ pub trait Adapter {
     fn adapt(&mut self, v: Option<Result<Vec<u8>, RcErr>>) -> Option<Self::Item>;
 }
 
-/**
-Simpler, less flexible, version of the [`Adapter`] trait.
-
-Can be used in situations where it suffices to just pass `None` and `Err()`
-values through and only operate when the inner [`ByteChunker`]'s `.next()`
-returns `Some(Ok(vec))`.
-*/
-pub trait SimpleAdapter {
-    type Item;
-
-    fn adapt(&mut self, v: Vec<u8>) -> Self::Item;
-}
-
 pub struct CustomChunker<R, A> {
     chunker: ByteChunker<R>,
     adapter: A,
+}
+
+impl<R, A> CustomChunker<R, A> {
+    /// Return a reference to this struct's [`Adapter`].
+    pub fn adapter(&self) -> &A { &self.adapter }
+
+    /// Return a mutable reference to this struct's `Adapter`.
+    pub fn adapter_mut(&mut self) -> &mut A { &mut self.adapter }
 }
 
 impl<R, A> Iterator for CustomChunker<R, A>
@@ -50,12 +45,35 @@ where
     }
 }
 
-impl<R, A> Iterator for CustomChunker<R, A>
+/**
+Simpler, less flexible, version of the [`Adapter`] trait.
+
+Can be used in situations where it suffices to just pass `None` and `Err()`
+values through and only operate when the inner [`ByteChunker`]'s `.next()`
+returns `Some(Ok(vec))`.
+
+This is less powerful than just using
+[`.map()`](https://doc.rust-lang.org/std/iter/trait.Iterator.html#method.map),
+_et. al._, but simpler because there's no error handling required by
+the custom type.
+*/
+pub trait SimpleAdapter {
+    type Item;
+
+    fn adapt(&mut self, v: Vec<u8>) -> Self::Item;
+}
+
+pub struct SimpleCustomChunker<R, S> {
+    chunker: ByteChunker<R>,
+    adapter: S,
+}
+
+impl<R, S> Iterator for SimpleCustomChunker<R, S>
 where
     R: Read,
-    A: SimpleAdapter,
+    S: SimpleAdapter,
 {
-    type Item = Result<A::Item, RcErr>;
+    type Item = Result<S::Item, RcErr>;
 
     fn next(&mut self) -> Option<Self::Item> {
         match self.chunker.next()? {
